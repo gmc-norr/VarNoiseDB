e2f1144930584b96e3d4f299472461b96a08fa6b
muabnezor <adam.rosenbaum@regionvasterbotten.se>
2025-03-05 16:32:08 +0100

    Use sqlalchemy

--- src/varnoisedb/database.py
+++ src/varnoisedb/database.py
@@ -3,7 +3,9 @@ This module handles database connections and operations for VarNoiseDB.
 It should support SQLite, PostgreSQL, and MySQL.
 """
 
-import sqlite3
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from varnoisedb.models import Base, Variant
 
 class DatabaseAdapter:
     def __init__(self, db_type='sqlite', db_name='variants.db', host=None, port=None, user=None, password=None):
@@ -13,63 +15,25 @@ class DatabaseAdapter:
         self.port = port or (5432 if db_type == 'postgresql' else 3306)
         self.user = user
         self.password = password
-        self.connection = self._connect()
-        self._create_table()
-        self._create_indices()
+        self.engine = self._create_engine()
+        self.Session = sessionmaker(bind=self.engine)
+        self._create_tables()
     
-    def _connect(self):
+    def _create_engine(self):
         if self.db_type == 'sqlite':
-            return sqlite3.connect(self.db_name)
+            return create_engine(f'sqlite:///{self.db_name}')
         elif self.db_type == 'postgresql':
-            import psycopg2
-            return psycopg2.connect(dbname=self.db_name, user=self.user, password=self.password, host=self.host, port=self.port)
+            return create_engine(f'postgresql://{self.user}:{self.password}@{self.host}:{self.port}/{self.db_name}')
         elif self.db_type == 'mysql':
-            import mysql.connector
-            return mysql.connector.connect(database=self.db_name, user=self.user, password=self.password, host=self.host, port=self.port)
+            return create_engine(f'mysql+mysqlconnector://{self.user}:{self.password}@{self.host}:{self.port}/{self.db_name}')
         else:
             raise ValueError(f"Unsupported database type: {self.db_type}")
     
-    def _create_table(self):
-        self.execute('''
-        CREATE TABLE IF NOT EXISTS variants (
-            chr TEXT,
-            pos INTEGER,
-            mean_non_ref_af REAL,
-            max_non_ref_af REAL,
-            min_non_ref_af REAL,
-            total_depth REAL,
-            number_of_samples INTEGER,
-            samples TEXT
-        )
-        ''')
+    def _create_tables(self):
+        Base.metadata.create_all(self.engine)
     
-    def _create_indices(self):
-        self.execute('''
-        CREATE INDEX IF NOT EXISTS idx_variants_chr_pos ON variants (chr, pos)
-        ''')
-    
-    def execute(self, query, params=None):
-        cursor = self.connection.cursor()
-        if params:
-            cursor.execute(query, params)
-        else:
-            cursor.execute(query)
-        self.connection.commit()
-        return cursor
-    
-    def executemany(self, query, params_list):
-        cursor = self.connection.cursor()
-        cursor.executemany(query, params_list)
-        self.connection.commit()
-        return cursor
-    
-    def fetchone(self, query, params=None):
-        cursor = self.connection.cursor()
-        if params:
-            cursor.execute(query, params)
-        else:
-            cursor.execute(query)
-        return cursor.fetchone()
+    def get_session(self):
+        return self.Session()
     
     def close(self):
-        self.connection.close()
+        self.engine.dispose()
--- src/varnoisedb/updater.py
+++ src/varnoisedb/updater.py
@@ -4,6 +4,8 @@ It calculates and updates the mean, max, and min allele frequencies.
 """
 
 import json
+from sqlalchemy.orm import sessionmaker
+from varnoisedb.models import Variant
 
 class Updater:
     def __init__(self, db_adapter, gvcf_parser, batch_size=1000):
@@ -12,49 +14,49 @@ class Updater:
         self.batch_size = batch_size
     
     def update_database(self):
+        session = self.db_adapter.get_session()
         batch = []
         for chr, pos, non_ref_af, dp, sample_name in self.gvcf_parser.parse():
             batch.append((chr, pos, non_ref_af, dp, sample_name))
             if len(batch) >= self.batch_size:
-                self._execute_batch(batch)
+                self._execute_batch(session, batch)
                 batch.clear()
         
         if batch:
-            self._execute_batch(batch)
+            self._execute_batch(session, batch)
+        
+        session.commit()
+        session.close()
     
-    def _execute_batch(self, batch):
+    def _execute_batch(self, session, batch):
         positions = [(chr, pos) for chr, pos, _, _, _ in batch]
-        placeholders = ', '.join(['(?, ?)'] * len(positions))
-        query = f'''
-        SELECT chr, pos, mean_non_ref_af, max_non_ref_af, min_non_ref_af, total_depth, number_of_samples, samples
-        FROM variants WHERE (chr, pos) IN ({placeholders})
-        '''
-        existing_records = self.db_adapter.execute(query, [item for sublist in positions for item in sublist]).fetchall()
-        existing_dict = {(chr, pos): (mean_non_ref_af, max_non_ref_af, min_non_ref_af, total_depth, number_of_samples, samples)
-                         for chr, pos, mean_non_ref_af, max_non_ref_af, min_non_ref_af, total_depth, number_of_samples, samples in existing_records}
+        existing_records = session.query(Variant).filter(
+            (Variant.chr, Variant.pos).in_(positions)
+        ).all()
+        existing_dict = {(record.chr, record.pos): record for record in existing_records}
         
         for record in batch:
             chr, pos, non_ref_af, dp, sample_name = record
             if (chr, pos) in existing_dict:
-                old_mean_non_ref_af, old_max_non_ref_af, old_min_non_ref_af, old_total_depth, old_number_of_samples, old_samples = existing_dict[(chr, pos)]
-                new_number_of_samples = old_number_of_samples + 1
-                new_total_depth = old_total_depth + dp
-                new_mean_non_ref_af = ((old_mean_non_ref_af * old_number_of_samples) + non_ref_af) / new_number_of_samples
-                new_max_non_ref_af = max(old_max_non_ref_af, non_ref_af)
-                new_min_non_ref_af = min(old_min_non_ref_af, non_ref_af)
-                samples = json.loads(old_samples)
+                variant = existing_dict[(chr, pos)]
+                variant.number_of_samples += 1
+                variant.total_depth += dp
+                variant.mean_non_ref_af = ((variant.mean_non_ref_af * (variant.number_of_samples - 1)) + non_ref_af) / variant.number_of_samples
+                variant.max_non_ref_af = max(variant.max_non_ref_af, non_ref_af)
+                variant.min_non_ref_af = min(variant.min_non_ref_af, non_ref_af)
+                samples = json.loads(variant.samples)
                 if sample_name not in samples:
                     samples.append(sample_name)
-                new_samples = json.dumps(samples)
+                variant.samples = json.dumps(samples)
             else:
-                new_number_of_samples = 1
-                new_total_depth = dp
-                new_mean_non_ref_af = non_ref_af
-                new_max_non_ref_af = non_ref_af
-                new_min_non_ref_af = non_ref_af
-                new_samples = json.dumps([sample_name])
-            
-            self.db_adapter.execute('''
-            INSERT OR REPLACE INTO variants (chr, pos, mean_non_ref_af, max_non_ref_af, min_non_ref_af, total_depth, number_of_samples, samples)
-            VALUES (?, ?, ?, ?, ?, ?, ?, ?)
-            ''', (chr, pos, new_mean_non_ref_af, new_max_non_ref_af, new_min_non_ref_af, new_total_depth, new_number_of_samples, new_samples))
+                variant = Variant(
+                    chr=chr,
+                    pos=pos,
+                    mean_non_ref_af=non_ref_af,
+                    max_non_ref_af=non_ref_af,
+                    min_non_ref_af=non_ref_af,
+                    total_depth=dp,
+                    number_of_samples=1,
+                    samples=json.dumps([sample_name])
+                )
+                session.add(variant)
